d16f01dcbfa774400bfa25a23faac7e75c779aec
Amir Mohammad Moradi <43685288+Amirmoradi94@users.noreply.github.com>
2021-08-22 22:43:58 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,102 @@
+
+from flask import Flask,jsonify
+from flask.globals import request
+from filter_ import octavefilter
+#from test_func import amir
+#import numpy as np
+#import timeit
+from threading import Thread
+#import concurrent.futures
+#from multiprocessing.pool import ThreadPool
+
+
+app = Flask(__name__) #intance of our flask application 
+
+#Route '/' to facilitate get request from our flutter app
+@app.route("/api", methods=["POST"])
+def function():
+    
+    request_data = request.get_json()
+    dataset = request_data["dataset"]
+    
+
+    #"""
+    x.append(dataset[0])
+    y.append(dataset[1])
+    z.append(dataset[2])
+    
+    #"""
+    #start = timeit.timeit()
+    
+    
+    """
+    pool = ThreadPool(processes=1)
+    x_thread = pool.apply_async(octavefilter, x)
+    return_val = x_thread.get()
+    """
+    
+    
+    class GetTitleThread(Thread):  
+        def __init__(self, data):
+            self.output = 0
+            self.dataa = data
+            super(GetTitleThread, self).__init__()
+            
+        def run(self):
+            self.output = octavefilter(self.dataa)
+    
+    
+    thread_x = GetTitleThread(x)
+    #thread_y = GetTitleThread(y)
+    #thread_z = GetTitleThread(z)
+    
+    thread_x.start()
+    #thread_y.start()
+    #thread_z.start()
+    
+    thread_x.join()
+    #thread_y.join()
+    #thread_z.join()
+    
+    
+    xx = thread_x.output
+    #yy = thread_y.output
+    #zz = thread_z.output
+    
+        
+    
+    #x_thread = threading.Thread(target=octavefilter, args=(x, ))
+    #y_thread = threading.Thread(target=octavefilter, args=(y, ))
+    
+    #x_thread.start()
+    #y_thread.start()
+    
+    #x_thread.join()
+    #y_thread.join()
+
+    #stop = timeit.timeit()
+    #print(stop-start)
+    #"""
+
+
+    #"""
+    #print(np.sqrt(np.square(xx) + np.square(yy) + np.square(zz)))
+    
+    
+    output = {"threshold": str(1.5)}
+    return jsonify(output)
+    
+
+if __name__ == "__main__":
+        
+    x , y, z = [], [], []
+    #host= '192.168.2.23'
+    
+    app.run(threaded=True, port=5000)
+
+    
+    
+    
+    
+    
+    
---
+++ filter_.py
@@ -0,0 +1,130 @@
+
+import numpy as np
+from scipy import signal
+
+
+
+#"""
+def _initindex(f, fr, g, b):
+    if b % 2:  # ODD ('x' solve from ANSI s1.11, eq. 3)
+        return np.round(
+                (b * np.log(f / fr) + 30 * np.log(g)) / np.log(g)
+                )
+    else:  # EVEN ('x' solve from ANSI s1.11, eq. 4)
+        return np.round((2 * b * np.log(f / fr) + 59 * np.log(g)) / (2 * np.log(g)))  
+
+
+def _ratio(g, x, b):
+    if b % 2:  # ODD (ANSI s1.11, eq. 3)
+        return g ** ((x - 30) / b)
+    else:  # EVEN (ANSI s1.11, eq. 4)
+        return g ** ((2 * x - 59) / (2 * b))
+
+def _bandedge(g, b):
+    # Band-edge ratio (ANSI s1.11, 3.7, pg. 3)
+    return g ** (1 / (2 * b))
+
+
+def getansifrequencies(fraction, limits=None):
+
+    if limits is None:
+        limits = [4, 2000]
+    
+    # Octave ratio g (ANSI s1.11, 3.2, pg. 2)
+    g = 10 ** (3 / 10)  # Or g = 2
+    # Reference frequency (ANSI s1.11, 3.4, pg. 2)
+    fr = 1000
+    
+    # Get starting index 'x' and first center frequency
+    x = _initindex(limits[0], fr, g, fraction)
+    freq = _ratio(g, x, fraction) * fr
+    
+    # Get each frequency until reach maximum frequency
+    freq_x = 0
+    while freq_x * _bandedge(g, fraction) < limits[1]:
+        # Increase index
+        x = x + 1
+        # New frequency
+        freq_x = _ratio(g, x, fraction) * fr
+        # Store new frequency
+        freq = np.append(freq, freq_x)
+    # Get band-edges
+    freq_d = freq / _bandedge(g, fraction)
+    freq_u = freq * _bandedge(g, fraction)
+    return freq.tolist(), freq_d.tolist(), freq_u.tolist()    
+
+
+def _deleteouters(freq, freq_d, freq_u, fs):
+    idx = np.asarray(np.where(np.array(freq_u) > fs / 2))
+    if any(idx[0]):
+        #_printwarn('Low sampling rate, frequencies above fs/2 will be removed')
+        freq = np.delete(freq, idx).tolist()
+        freq_d = np.delete(freq_d, idx).tolist()
+        freq_u = np.delete(freq_u, idx).tolist()
+    return freq, freq_d, freq_u
+
+
+def _genfreqs(limits, fraction, fs):
+    # Generate frequencies
+    freq, freq_d, freq_u = getansifrequencies(fraction, limits)
+    
+    # Remove outer frequency to prevent filter error (fs/2 < freq)
+    freq, freq_d, freq_u = _deleteouters(freq, freq_d, freq_u, fs)
+    
+    return freq, freq_d, freq_u
+
+
+def _thirdoctave():
+    # IEC 61260 - 1 - 2014 (added 12.5, 16, 20 Hz)
+    return [4, 5, 6.3, 8, 10, 12.5, 16, 20, 25, 31.5, 40, 50, 63, 80, 100, 
+            125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000]
+
+
+
+def _downsamplingfactor(freq, fs):
+    guard = 0.10
+    factor = (np.floor((fs / (2+guard)) / np.array(freq))).astype('int')
+    for idx in range(len(factor)):
+        # Factor between 1<factor<50
+        factor[idx] = max(min(factor[idx], 50), 1)
+    return factor
+
+
+def octavefilter(x, fs=4500, fraction=3, order=6, limits=[4, 2000]):
+    
+    octave_weights = [0.375, 0.545, 0.727, 0.873, 0.951, 0.958, 0.896, 0.782, 0.647, 0.519, 0.411, 0.324, 0.256, 0.202, 0.160, 0.127, 0.101, 0.0799, 0.0634, 0.0503, 0.0398, 0.0314, 0.0245, 0.0186, 0.0135, 0.00894, 0.00536, 0.00295]
+
+    
+    if limits is None:
+        limits = [4, 2000]
+    
+    freq, freq_d, freq_u = _genfreqs(limits, fraction, fs)
+    
+    factor = _downsamplingfactor(freq_u, fs)
+    
+    
+    sos = [[[]] for i in range(len(freq))]
+    
+    
+    for idx, (lower, upper) in enumerate(zip(freq_d, freq_u)):
+        
+        fsd = fs / factor[idx]  # New sampling rate
+        sos[idx] = signal.butter(N = order, 
+                      Wn = np.array([lower, upper]) / (fsd / 2), 
+                      btype = 'bandpass', 
+                      fs = 4500,
+                      output='sos')
+    
+
+    # Create array with SPL for each frequency band
+    spl = np.zeros([len(freq)])
+    for idx in range(len(freq)):
+        sd = signal.decimate(x, factor[idx])
+        
+        y = signal.sosfilt(sos[idx], sd)
+        
+        spl[idx] = np.sqrt(np.mean(np.square(y)))
+    
+    weighted_output = np.array(octave_weights) * np.array(spl)
+    
+    return np.sqrt(np.sum(np.square(weighted_output)))
